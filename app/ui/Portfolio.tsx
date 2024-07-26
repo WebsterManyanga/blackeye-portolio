b8eca26375dd9be50dfd94e6b00f8106e8496e83
Mac <mac@macbook-pro-mac.home>
2024-07-26 21:25:26 +0200

    added video

--- app/ui/Portfolio.tsx
+++ app/ui/Portfolio.tsx
@@ -44,7 +44,10 @@ const Portfolio = () => {
         your own narrative&quot;
       </p>
       <div className=" mt-10 relative md:w-[80%] lg:w-[60%]">
-        <Image src={photoAlbum[47].image} alt="photo" width={4000} height={5000} />
+        {/* <Image src={photoAlbum[47].image} alt="photo" width={4000} height={5000} /> */}
+        <video className="w-full md:w-[70%]" autoPlay loop muted>
+          <source src="/videos/fortBema.mov" type="video/mp4" />
+        </video>
       </div>
       </div>
       </div>
