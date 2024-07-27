8d3f58287cbdeb11e27232fc8b631fd8d4bed25d
Mac <mac@macbook-pro-mac.home>
2024-07-27 21:59:22 +0200

    updated scroll snap to show on md to lg devices

--- app/ui/Latest.tsx
+++ app/ui/Latest.tsx
@@ -20,7 +20,7 @@ const Latest = () => {
   ));
 
   return (
-    <section className=" pt-10 px-5 snap-start">
+    <section className=" pt-10 px-5 md:snap-start">
       <h1 className={`${cinzel.className} text-5xl text-primary`}>
         <span className={cinzelDecorative.className}>O</span>ur{" "}
         <span className={cinzelDecorative.className}>L</span>atest{" "}
